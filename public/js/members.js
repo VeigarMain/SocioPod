ed710529c3cc88810968a34ee5005ea68b8127d0
Jake Battaglia <jbaked12@gmail.com>
2020-09-23 23:55:03 -0400

    added title of podcasts, added img, and descrip

--- public/js/members.js
+++ public/js/members.js
@@ -3,17 +3,37 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/members/:intOne/:intTwo/:intThree").then(data => {
       console.log(data);
+      // fixed members.js get route to append data from api to our members page!
     const result = document.getElementById("result");
     data.results.map(item => {
       console.log(item.audio);
+      console.log(item.title_original);
       const node = document.createElement("a");
       const textNode = document.createTextNode(item.audio);
       node.appendChild(textNode);
-      // const audio = `<p>${item[0].audio}</p>`;
       node.href = item.audio;
+
+      // This is for showing the title of the specific url
+      const newTitle = document.createElement("p");
+      const textTitle = document.createTextNode(item.title_original);
+      newTitle.appendChild(textTitle);
+      result.appendChild(newTitle);
+      // const audio = `<p>${item[0].audio}</p>`;
       const br = document.createElement("br");
       node.appendChild(br);
       result.appendChild(node);
+
+      const imgDiv = document.createElement("div");
+      const newImg = document.createElement("img");
+      newImg.src = item.image;
+      imgDiv.appendChild(newImg);
+      result.appendChild(imgDiv);
+
+      const newDescrip = document.createElement("p");
+      const description = document.createTextNode(item.description_original);
+      newDescrip.appendChild(description);
+      result.appendChild(newDescrip);
+
     });
   });
   // $("button").on("click", event => {
