126a48f2fac5080cee6f28765d534a6ecfe83e88
Jake Battaglia <jbaked12@gmail.com>
2020-09-24 18:38:02 -0400

    adding styling to the members page and tried to add more functionallity

--- public/js/members.js
+++ public/js/members.js
@@ -6,31 +6,45 @@ $(document).ready(() => {
     // fixed members.js get route to append data from api to our members page!
     const result = document.getElementById("result");
     data.results.map(item => {
+      const newDiv = document.createElement("div");
+      const addCard = document.createTextNode(item.audio + item.title_original + item.description_original + item.image);
+      newDiv.appendChild(addCard);
+
+
       const node = document.createElement("a");
       const textNode = document.createTextNode(item.audio);
+      const newElement = document.createElement("div");
+      newElement.classList.add("holderdiv");
       node.appendChild(textNode);
       node.href = item.audio;
-       
+
+      // this is for making cards for the specific podcast with descriptions and img...
+      const newTitle = document.createElement("h2");
       // This is for showing the title of the specific url
-      const newTitle = document.createElement("p");
       const textTitle = document.createTextNode(item.title_original);
       newTitle.appendChild(textTitle);
-      result.appendChild(newTitle);
+      newElement.appendChild(newTitle);
       // const audio = `<p>${item[0].audio}</p>`;
       const br = document.createElement("br");
       node.appendChild(br);
-      result.appendChild(node);
+      newElement.appendChild(node);
       const imgDiv = document.createElement("div");
       const newImg = document.createElement("img");
-      
+
       newImg.src = item.image;
       imgDiv.appendChild(newImg);
-      result.appendChild(imgDiv);
+      newElement.appendChild(imgDiv);
 
       const newDescrip = document.createElement("p");
       const description = document.createTextNode(item.description_original);
       newDescrip.appendChild(description);
-      result.appendChild(newDescrip);
+      newElement.appendChild(newDescrip);
+      result.appendChild(newElement);
+
+
+
+
+
     });
   });
   // $("button").on("click", event => {
