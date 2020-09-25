0ce4dbe5e78149528e7c7bea1f256522bc704f53
Matt Stalzer <mjs58181@uga.edu>
2020-09-25 15:41:00 -0400

    switching branches

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,13 +10,17 @@ module.exports = function(app) {
   });
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
-
+    if (req.user) {
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
-
+    if (req.user) {
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
